24c70a90b0aad8a31ff5ae306f58960b9bac3165
Akhil Akkapelli <akhilakkapelli28@gmail.com>
2026-02-20 15:53:42 +0530

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+public:
+	int rollNumber;
+	string name;
+	float marks;
+};
+
+int main() {
+	Student s;
+	cout << "Enter roll number: ";
+	cin >> s.rollNumber;
+	cin.ignore();
+	cout << "Enter name: ";
+	getline(cin, s.name);
+	cout << "Enter marks: ";
+	cin >> s.marks;
+
+	cout << "\nStudent Details:\n";
+	cout << "Roll Number: " << s.rollNumber <<endl;
+	cout << "Name: " << s.name << endl;
+	cout << "Marks: " << s.marks << endl;
+
+	return 0;
+}
+
